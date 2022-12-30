5e998187ae7540d9e68f92403f09e314febb5008
AhmedFady775 <fadiahmed298@gmail.com>
2022-12-30 04:54:18 +0200

    1

--- middleware/auth.js
+++ middleware/auth.js
@@ -20,6 +20,14 @@ import jwt from "jsonwebtoken";
 //     }
 // };
 
+export const isAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(401).send({ message: 'Invalid Admin Token' });
+    }
+};
+
 export const isAuth = (req, res, next) => {
     const authorization = req.headers.authorization;
     if (authorization) {
