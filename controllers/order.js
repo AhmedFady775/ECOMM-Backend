a698a5a9ba2dc6e64fc3cc56b63b7cee61564dc5
AhmedFady775 <fadiahmed298@gmail.com>
2023-01-01 17:41:11 +0200

    1

--- controllers/order.js
+++ controllers/order.js
@@ -37,4 +37,44 @@ export const getOrder = async (req, res) => {
 }
 
 
+export const summary = async (req, res) => {
+    const orders = await Order.aggregate([
+        {
+            $group: {
+                _id: null,
+                numOrders: { $sum: 1 },
+                totalSales: { $sum: '$totalPrice' },
+            },
+        },
+    ]);
+    const users = await User.aggregate([
+        {
+            $group: {
+                _id: null,
+                numUsers: { $sum: 1 },
+            },
+        },
+    ]);
+    const dailyOrders = await Order.aggregate([
+        {
+            $group: {
+                _id: { $dateToString: { format: '%Y-%m-%d', date: '$createdAt' } },
+                orders: { $sum: 1 },
+                sales: { $sum: '$totalPrice' },
+            },
+        },
+        { $sort: { _id: 1 } },
+    ]);
+    const productCategories = await Product.aggregate([
+        {
+            $group: {
+                _id: '$category',
+                count: { $sum: 1 },
+            },
+        },
+    ]);
+    res.send({ users, orders, dailyOrders, productCategories });
+}
+
+
 
--- routes/order.js
+++ routes/order.js
@@ -1,14 +1,14 @@
 import express from "express";
-import { getOrder, getOrders, addOrder, getMyOrders } from "../controllers/order.js";
-import { isAuth } from "../middleware/auth.js";
+import { getOrder, getOrders, addOrder, getMyOrders, summary } from "../controllers/order.js";
+import { isAdmin, isAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/", getOrders)
+router.get("/", isAuth, isAdmin, getOrders)
 router.get("/mine", isAuth, getMyOrders)
 router.post("/create", isAuth, addOrder)
-router.get("/:id", getOrder)
-
+router.get("/:id", isAuth, getOrder)
+router.get("/summary", isAuth, isAdmin, summary)
 
 
 export default router;
