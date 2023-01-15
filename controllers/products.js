26cad181df2142f9e843fbf9f1e30ad99c199956
AhmedFady775 <fadiahmed298@gmail.com>
2023-01-15 11:23:43 +0200

    1

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,10 @@
 import Product from "../models/Product.js";
 
-const PAGE_SIZE = 5;
 
-export const getProducts = async (req, res) => {
+export const getProductsPaged = async (req, res) => {
     try {
         const page = req.query.page || 1;
-        const perPage = req.query.perPage || 5;
+        const perPage = req.query.perPage || 8;
         const count = await Product.countDocuments({});
         const products = await Product.find({})
             .skip((page - 1) * parseInt(perPage))
@@ -17,6 +16,15 @@ export const getProducts = async (req, res) => {
 };
 
 
+export const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find({})
+        const count = await Product.countDocuments({});
+        res.status(200).json({ products, count });
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
 
 
 
--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,11 @@
-import { addProduct, getProduct, removeProduct, editProduct, getProducts } from "../controllers/products.js";
+import { addProduct, getProduct, removeProduct, editProduct, getProducts, getProductsPaged } from "../controllers/products.js";
 import { isAdmin, isAuth } from "../middleware/auth.js";
 import express from "express";
 
 const router = express.Router();
 
-router.get("/", getProducts)
+router.get("/allproducts", getProducts)
+router.get("/", getProductsPaged)
 router.post("/create", addProduct)
 router.get("/:id", getProduct)
 router.delete("/:id", isAuth, isAdmin, removeProduct)
