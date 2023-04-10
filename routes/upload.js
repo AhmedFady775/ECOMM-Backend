aa5356285e479cb7d9b55c8ae5e7a2b281eabc39
Ahmed Mohamed Fady <fadiahmed298@gmail.com>
2023-04-10 13:49:42 +0200

    updates

--- routes/upload.js
+++ routes/upload.js
@@ -1,24 +1,38 @@
+import express from 'express';
+import multer from 'multer';
+import { v2 as cloudinary } from 'cloudinary';
+import streamifier from 'streamifier';
+import { isAdmin, isAuth } from '../utils.js';
 
-const express = require("express");
-const multer = require("multer");
-const { isAuth, isAdmin } = require("../middleware/auth.js");
+const upload = multer();
 
-const storage = multer.diskStorage({
-    destination(req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename(req, file, cb) {
-        cb(null, `${Date.now()}`);
-    },
-});
+const uploadRouter = express.Router();
 
-const upload = multer({ storage });
-const router = express.Router();
-
-router.post('/', isAuth, isAdmin, upload.single('image'), (req, res) => {
-    res.status(201).send({ image: `/${req.file.path}` });
-});
-module.exports = router;
-
-
-
+uploadRouter.post(
+    '/',
+    isAuth,
+    isAdmin,
+    upload.single('file'),
+    async (req, res) => {
+        cloudinary.config({
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET,
+        });
+        const streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                const stream = cloudinary.uploader.upload_stream((error, result) => {
+                    if (result) {
+                        resolve(result);
+                    } else {
+                        reject(error);
+                    }
+                });
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+        const result = await streamUpload(req);
+        res.send(result);
+    }
+);
+export default uploadRouter;
